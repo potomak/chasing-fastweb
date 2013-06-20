62d5fc9a4a7e72c16cb3221fa87cedf504411251
Giovanni Cappellotto <potomak84@gmail.com>
2013-06-21 01:17:38 +0200

    Implement Tree component

--- components/tree.lua
+++ components/tree.lua
@@ -1,4 +1,22 @@
 -- Tree class
 
 Tree = {}
+Tree.__index = Tree
 
+-- Constructor
+function Tree:new()
+  -- define our parameters here
+  local tree = {
+    x = 0,
+    y = 0,
+    scale = 1,
+    image = nil
+  }
+
+  return setmetatable(tree, Tree)
+end
+
+function Tree:draw()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(self.image, self.x, self.y, 0, self.scale)
+end
