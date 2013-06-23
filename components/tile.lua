87ee72ceebc8926740a47ded720313acbf0affef
Giovanni Cappellotto <potomak84@gmail.com>
2013-06-24 00:03:09 +0200

    Set dynamic tile scale factor

--- components/tile.lua
+++ components/tile.lua
@@ -4,10 +4,9 @@ Tile = class('Tile')
 -- Constructor
 function Tile:initialize()
   self.image = {}
-  self.scale = 4
 end
 
 function Tile:draw(x, y)
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(self.image, x, y, 0, self.scale)
+  love.graphics.draw(self.image, x, y, 0, Map.TILE_SIZE/self.image:getWidth())
 end
