646244e95ecc8f63e8a0d070cf1afc3b602c6200
Giovanni Cappellotto <potomak84@gmail.com>
2013-06-25 00:10:46 +0200

    Implement Truck class

--- components/truck.lua
+++ components/truck.lua
@@ -1,5 +1,20 @@
 -- Truck class
 Truck = class('Truck', Component)
 
-function Truck:initialize()
+function Truck:initialize(width, height, x, y)
+  Component.initialize(self, width, height)
+  self.x = x
+  self.y = y
+  self.image = nil
+end
+
+function Truck:draw()
+  if world.showTruck then
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(self.image, self.x, self.y, 0, 4)
+  end
+end
+
+function Truck:update(dt)
+  self.x = self.x + (world.stageSpeed * dt)
 end
